3f09a10580e597a22d50442f8bcf819561b4869d
noxiouz <noxiouz@cocaine-cloud01g.kit.yandex.net>
2012-12-10 02:17:25 +0400

    work on setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,18 @@ try:
 except ImportError:
     from distutils.core import setup, Extension
 
-setup( name = "Combaine",
+setup(
+        name = "Combaine",
+        packages = [
+        'combaine',
+        'combaine.combainer',
+        'combaine.combainer.Schedule',
+        'combaine.combainer.Observer',
+        'combaine.plugins.Aggregators',
+        'combaine.plugins.DataGrid',
+        'combaine.plugins.DistributedStorage',
+        'combaine.plugins.DataFetcher',
+        'combaine.plugins.StorageAPI',
+        'combaine.plugins.LockServerAPI',
+        ]
 )
