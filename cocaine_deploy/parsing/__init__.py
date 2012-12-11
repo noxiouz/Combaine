78144d5419e35807d95db893b23e2ac3318ccd1d
noxiouz <noxiouz@yandex.ru>
2012-12-11 15:06:29 +0400

    fix imports in cloud apps

--- cocaine_deploy/parsing/__init__.py
+++ cocaine_deploy/parsing/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import sys
-sys.path = sys.path+['/usr/lib/yandex/combaine/']
-from parsing import parsing
+#sys.path = sys.path+['/usr/lib/yandex/combaine/']
+from combaine.cloud.parsing import parsing
 
