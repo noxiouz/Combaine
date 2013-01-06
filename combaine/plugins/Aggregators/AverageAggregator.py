d9e8a40b82db90f98c9f4142952386ef32cebbac
noxiouz <noxiouz@yandex.ru>
2013-01-06 23:12:35 +0400

    Add handling exception in Aggreagtors

--- combaine/plugins/Aggregators/AverageAggregator.py
+++ combaine/plugins/Aggregators/AverageAggregator.py
@@ -12,12 +12,23 @@ class AverageAggregator(AbstractAggregator):
         print self.query
 
     def aggregate(self, db, timeperiod):
+        def format_me(i):
+            try:
+                ret = i[0][0]
+            except Exception:
+                pass
+            else:
+                return ret
+
         self.query = self.table_regex.sub(db.tablename, self.query)
         if self.time_regex.search(self.query):
             queries = ((self.time_regex.sub(str(time), self.query), time) for time in xrange(*timeperiod))
         else:
             queries = (self.query, timeperiod[1])
-        return ({ 'name' : self.name, 'time': time, 'data' : db.perfomCustomQuery(query)[0][0]} for query, time in queries)
+        l = ((format_me(db.perfomCustomQuery(query)), _time) for query, _time in queries)
+        return ({ 'name' : self.name, 'time': time, 'data' : res} for res, time in l if res is not None)
+
+
 
 
 PLUGIN_CLASS = AverageAggregator
