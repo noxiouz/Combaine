51c80dc54d1bb573149b8e12d0884ecc5e3b9a48
noxiouz <noxiouz@yandex.ru>
2013-02-25 14:40:22 +0400

    Exclude elliptics proxy on timeout error

--- combaine/plugins/DistributedStorage/elliptics_http.py
+++ combaine/plugins/DistributedStorage/elliptics_http.py
@@ -14,7 +14,7 @@ from time import sleep
 class Elliptics(AbstractDistributedStorage):
 
     def __init__(self, **config):
-        cfg = [_i.split(":") for _i in config["proxy_hosts"]]
+        cfg = [tuple(_i.split(":")) for _i in config["proxy_hosts"]]
         random.shuffle(cfg)
         self.hostsinfo = cfg
         self.read_url = string.Template("http://${HOST}:${R_PORT}/get/${KEY}?ioflags=3072")
@@ -25,30 +25,31 @@ class Elliptics(AbstractDistributedStorage):
 
     def insert(self, key, data):
         key =  hashlib.md5(key).hexdigest()
-        for host, r_port, w_port in self.hostsinfo:
+        for host, r_port, w_port in self.hostsinfo[:]:
             try:
-                r = requests.post(self.write_url.substitute(KEY=key, HOST=host, W_PORT=w_port), data=PACK(data), timeout=3)
+                r = requests.post(self.write_url.substitute(KEY=key, HOST=host, W_PORT=w_port), data=PACK(data), timeout=1)
                 if r.status_code == 503: #because elliptics write cache bug
                     return True
             except requests.exceptions.Timeout as err:
-                pass
+                self.hostsinfo.remove((host, r_port, w_port))
             except requests.exceptions.ConnectionError as err:
-                pass
+                print self.hostsinfo
+                self.hostsinfo.remove((host, r_port, w_port))
         return False
 
     def read(self, key, cache=False):
         key =  hashlib.md5(key).hexdigest()
-        for host, r_port, w_port in self.hostsinfo:
+        for host, r_port, w_port in self.hostsinfo[:]:
             try:
-                r = requests.post(self.read_url.substitute(KEY=key, HOST=host, R_PORT=r_port), timeout=3)
+                r = requests.post(self.read_url.substitute(KEY=key, HOST=host, R_PORT=r_port), timeout=1)
                 if r.ok:
                     ret = UNPACK(r.content)
                     r.close()
                     return ret
             except requests.exceptions.Timeout as err:
-                print str(err)
+                self.hostsinfo.remove((host, r_port, w_port))
             except requests.exceptions.ConnectionError as err:
-                print str(err)
+                self.hostsinfo.remove((host, r_port, w_port))
             except Exception as err:
                 print str(err)
         return []
