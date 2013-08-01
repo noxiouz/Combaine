76345c4c53d7d2c859655f44c38cc69ea3bf04ac
noxiouz <noxiouz@yandex.ru>
2013-08-01 13:00:00 +0400

    Juggler childs

--- combaine/plugins/Senders/juggler.py
+++ combaine/plugins/Senders/juggler.py
@@ -54,6 +54,7 @@ class Juggler(AbstractSender):
         self._WARNING = cfg.get("WARNING", [])
         self._CRITICAL = cfg.get("CRITICAL", [])
         self.checkname = cfg["checkname"]
+        self.Host = cfg['Host']
         self.description = cfg.get("description", "no description")
         self._OK = cfg.get("OK", [])
         self._aggs = list()
@@ -67,61 +68,73 @@ class Juggler(AbstractSender):
 
     def _handling_one_expression(self, level, data, name, status):
         for expression in level:
-                code = expression
-                for key, value in data.iteritems():
-                    code, n = re.subn(r"\${%s}" % key, str(value), code)
+            code = expression
+            for key, value in data.iteritems():
+                code, n = re.subn(r"\${%s}" % key, str(value), code)
+            try:
+                res = eval(code)
+                self.logger.debug("After substitution in %s %s %s" % (name, code, res))
+            except Exception as err:
+                res = False
+            if res:
+                self._add_check_if_needed(name)
+                cmd = ("juggler_queue_event", "--host=" + name, "-n", self.checkname, "-s", str(status), "-d", self.description)
                 try:
-                    res = eval(code)
-                    self.logger.debug("After substitution in %s %s %s" % (name, code, res))
-                except Exception as err:
-                    res = False
-                if res:
-                    self._add_check_if_needed(name)
-                    cmd = ("juggler_queue_event", "--host=" + name, "-n", self.checkname, "-s", str(status), "-d", self.description)
-                    try:
-                        self.logger.info(' '.join(cmd))
-                        subprocess.check_call(cmd)
-                    except subprocess.CalledProcessError as err:
-                        self.logger.error("Calling juggler client was failed")
-                    else:
-                        return True
+                    self.logger.info(' '.join(cmd))
+                    subprocess.check_call(cmd)
+                except subprocess.CalledProcessError:
+                    self.logger.error("Calling juggler client was failed")
+                else:
+                    return True
         return False
 
     def _add_check_if_needed(self, host):
         http_cli = AsyncHTTP()
-        #http_cli.fetch_any(dict((juggler_host,
-        #                         "http://%s/api/checks/list_checks?host_name=%s&do=1" % (juggler_host, host))
-        #                          for juggler_host in self.juggler_hosts), timeout=1)
-        params = {
-                "host": host,
-                "service": urllib.quote(self.checkname),
-                "description": urllib.quote(self.description),
-                "methods": "GOLEM" }
+        params = {"host": self.Host,
+                  "service": urllib.quote(self.checkname),
+                  "description": urllib.quote(self.description),
+                  "methods": "GOLEM",
+                  "child": host}
 
         add_check_urls = dict((juggler_host,
                                 "http://%s" % juggler_host + 
                                 "/api/checks/set_check?host_name={host}&service_name={service}&description={description}&do=1".format(**params))
                                 for juggler_host in self.juggler_hosts)
+
+        add_children = dict((juggler_host,
+                                "http://%s" % juggler_host +
+                                "/api/checks/add_child?host_name={host}&service_name={service}&child={child}:{service}&do=1".format(**params))
+                                for juggler_host in self.juggler_hosts)
+
         add_methods_urls = dict((juggler_host,
                                 "http://%s" % juggler_host + 
                                 "/api/checks/add_methods?host_name={host}&service_name={service}&methods_list={methods}&do=1".format(**params))
                                 for juggler_host in self.juggler_hosts)
         
         http_cli.fetch_any(add_check_urls)
+        http_cli.fetch_any(add_children)
         http_cli.fetch_any(add_methods_urls)
-
-
-
 
     def send(self, data):
         interest_results = filter(lambda x: x.aggname in self._aggs, data)
         for subgroup_name, subgroup_data in make_template_placeholders(interest_results):
-            check_host_name = subgroup_name if subgroup_name == data[0].groupname\
-                                                 else "%s-%s" % (data[0].groupname, subgroup_name)
-            OK = self._handling_one_expression(self._CRITICAL, subgroup_data, check_host_name, 2) or\
-            self._handling_one_expression(self._WARNING, subgroup_data, check_host_name, 1) or\
-            self._handling_one_expression(self._INFO, subgroup_data, check_host_name, 3) or\
-            self._handling_one_expression(self._OK, subgroup_data, check_host_name, 0)
+            check_host_name = "%s-%s" % (self.Host, subgroup_name)\
+                              if subgroup_name == data[0].groupname\
+                              else "%s-%s-%s" % (self.Host,
+                                                 data[0].groupname,
+                                                 subgroup_name)
+            OK = self._handling_one_expression(self._CRITICAL,
+                                               subgroup_data,
+                                               check_host_name, 2) or\
+            self._handling_one_expression(self._WARNING,
+                                          subgroup_data,
+                                          check_host_name, 1) or\
+            self._handling_one_expression(self._INFO,
+                                          subgroup_data,
+                                          check_host_name, 3) or\
+            self._handling_one_expression(self._OK,
+                                          subgroup_data,
+                                          check_host_name, 0)
             if not OK:
                 self.logger.debug("Emit OK manually")
                 self._add_check_if_needed(check_host_name)
